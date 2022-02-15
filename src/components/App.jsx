46e37e62f5177a141167768eb1a467275868fff6
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-02-15 19:31:13 +0530

    new change

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const pi=3.14
+
+function App(){
+    return(
+     <div> 
+         <h1 className='margin'>Hello PI {pi} 
+         <button type="button" className="btn btn-lg btn-primary margin"><strong>Primary</strong></button>
+         </h1>
+         
+     </div>
+    );
+}
+
+export default App;
