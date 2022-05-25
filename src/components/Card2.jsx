1ccbb7f10e039c6165c2701ebfa36e6d44aef4e5
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-05-25 12:31:38 +0530

    update hashes

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -3,39 +3,38 @@ import { Button } from 'react-bootstrap';
 import Transaction from './Transaction';
 
 export default function Card2(){
-    return(
-     <div style={
-        {
-         height:"450px",
-         padding:"10px",
-         marginTop:"-20px",
-         backgroundColor:"white", 
-         borderRadius:"20px",
-         boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px"
-        }}> 
-        <h4 style={{marginBottom:"20px"}}>Latest Transactions</h4>
+    return (
+      <div
+        style={{
+          height: "450px",
+          padding: "10px",
+          marginTop: "-20px",
+          backgroundColor: "white",
+          borderRadius: "20px",
+          boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+        }}
+      >
+        <h4 style={{ marginBottom: "20px" }}>Latest Transactions</h4>
         <Transaction
-        key={1}
-        hash="84dd2763eb61b4fa56728fb61bb92ea041c8fa7287250b7619aebaad7b04c62b"
+          key={1}
+          hash="2dce3a5f12748b10c42ffd49496264a047b78db4dcaf776530fcf55aa63e44b1"
         />
         <Transaction
           key={2}
-        hash="514163fc8b19a1ffd9efde0f9bce61a95ae75f231ec5f76d8ba30c3c8fd540c3"
+          hash="a9d14d10416fadb3123528fc1810d5da244de3da08d53deb6db4dd52cb119a96"
         />
         <Transaction
           key={3}
-        hash="2af853a9ee47963e80ba117bd16b58252e3e94a773a9acdb1f96795183447880"/>
+          hash="d12fad37411110a68141216ae614a6a3d168e51ac16fd37bb61577c129e84cc6"
+        />
         <Transaction
           key={4}
-        hash="1225e20e9cbe031546a71b1e93f8c2ab5c0c300dd283c2e50112e7907cf8da97"/>
-        <div className='d-grid'>
-            <Button  
-           variant="primary">
-               View More
-        </Button>
+          hash="b1762321337b007bd922bcdcced0550479236f1ed0fcaa5d5a6d05f4e838521b"
+        />
+        <div className="d-grid">
+          <Button variant="primary">View More</Button>
         </div>
-          
-     </div>
+      </div>
     );
 }
 
